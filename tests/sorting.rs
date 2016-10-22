0434e53f6377429c9ba9b1b0153f552b99643ec3
Corentin Henry <corentinhenry@gmail.com>
2016-10-22 12:50:49 -0700

    test heap_sort

--- tests/sorting.rs
+++ tests/sorting.rs
@@ -3,6 +3,7 @@ extern crate aian;
 extern crate quickcheck;
 
 use aian::sorting::insertion_sort;
+use aian::sorting::heap_sort;
 
 #[test]
 fn insertion_sort_empty() {
@@ -36,6 +37,38 @@ quickcheck! {
 
         insertion_sort(&mut xs);
 
+        expected_sorted == xs
+    }
+}
+
+#[test]
+fn heap_sort_empty() {
+    let mut values: [i32; 0] = [];
+    heap_sort(&mut values);
+    assert_eq!(values, [])
+}
+
+#[test]
+fn heap_sort_one() {
+    let mut values = [1];
+    heap_sort(&mut values);
+    assert_eq!(values, [1]);
+}
+
+#[test]
+fn heap_multi() {
+    let mut values = [9, 8, 7, 11, 10];
+    heap_sort(&mut values);
+    let values_expected: Vec<_> = (7..12).collect();
+    assert_eq!(values_expected, values);
+}
+
+quickcheck! {
+    fn heap_everything(xs: Vec<i32>) -> bool {
+        let mut xs = xs;
+        let mut expected_sorted = xs.clone();
+        expected_sorted.sort();
+        heap_sort(&mut xs);
         expected_sorted == xs
     }
 }
