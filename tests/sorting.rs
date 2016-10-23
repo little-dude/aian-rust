a66798b9e457c6ad76c48f1a25ae8b1a7cd09e25
Corentin Henry <corentinhenry@gmail.com>
2016-10-23 16:55:56 -0700

    test quick sort

--- tests/sorting.rs
+++ tests/sorting.rs
@@ -2,8 +2,7 @@ extern crate aian;
 #[macro_use]
 extern crate quickcheck;
 
-use aian::sorting::insertion_sort;
-use aian::sorting::heap_sort;
+use aian::sorting::{heap_sort, insertion_sort, qsort};
 
 #[test]
 fn insertion_sort_empty() {
@@ -69,6 +68,38 @@ quickcheck! {
         let mut expected_sorted = xs.clone();
         expected_sorted.sort();
         heap_sort(&mut xs);
+        expected_sorted == xs
+    }
+}
+
+#[test]
+fn qsort_empty() {
+    let mut values: [i32; 0] = [];
+    qsort(&mut values);
+    assert_eq!(values, [])
+}
+
+#[test]
+fn qsort_one() {
+    let mut values = [1];
+    qsort(&mut values);
+    assert_eq!(values, [1]);
+}
+
+#[test]
+fn qsort_multi() {
+    let mut values = [9, 8, 7, 11, 10];
+    qsort(&mut values);
+    let values_expected: Vec<_> = (7..12).collect();
+    assert_eq!(values_expected, values);
+}
+
+quickcheck! {
+    fn qsort_everything(xs: Vec<i32>) -> bool {
+        let mut xs = xs;
+        let mut expected_sorted = xs.clone();
+        expected_sorted.sort();
+        qsort(&mut xs);
         expected_sorted == xs
     }
 }
